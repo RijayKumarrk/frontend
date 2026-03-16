7d6292a288831448ff9db2648a8890c8a3455db4
RIJAYKUMAR RK <153521062+RijayKumarrk@users.noreply.github.com>
2026-03-17 01:25:31 +0530

    Update API base URL to include '/api' suffix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://project-production-50c0.up.railway.app/', // <-- PASTE YOUR JAVA RAILWAY LINK HERE!
+  baseURL: 'https://project-production-50c0.up.railway.app/api', // <-- PASTE YOUR JAVA RAILWAY LINK HERE!
   withCredentials: true, 
 });
 
